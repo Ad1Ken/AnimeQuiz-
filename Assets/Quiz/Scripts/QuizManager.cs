d2490ed929259af5955c736aa25579e069876027
Kenji <56013517+Ad1Ken@users.noreply.github.com>
2021-03-11 23:19:11 +0530

    AnimeQuiz 1.0

---
+++ Assets/Quiz/Scripts/QuizManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField] private QuizUI quizUI;
+    [SerializeField]
+    private List<Question> questions;
+
+    private Question selectedQuestion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectQestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void selectQestion()
+    {
+        int val = Random.Range(0, questions.Count);
+        selectedQuestion = questions[val];
+        quizUI.SetQuestion(selectedQuestion);
+    }
+
+    public bool Answer(string answered)
+    {
+        bool correctAns = false;
+        
+        if(answered == selectedQuestion.correctAns)
+        {
+            correctAns = true;
+        }
+        else
+        {
+
+        }
+
+        Invoke("SelectQuestion", 0.4f);
+        return correctAns;
+    }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string questionInfo;
+    public List<string> options;
+    public QuestionType questionType;
+    public Sprite questionImg;
+    public AudioClip questionClip;
+    public UnityEngine.Video.VideoClip questionVideo;
+    public string correctAns;
+}
+
+[System.Serializable]
+public enum QuestionType
+{
+    TEXT,
+    IMAGE,
+    VIDEO,
+    AUDIO
+}
---
+++ Assets/Quiz/Scripts/QuizUI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizUI : MonoBehaviour
+{
+    [SerializeField] private QuizManager quizManager;
+    [SerializeField] private Text questionText;
+    [SerializeField] private Image questionImage;
+    [SerializeField] private UnityEngine.Video.VideoPlayer questionVid;
+    [SerializeField] private AudioSource questionAudio;
+    [SerializeField] private List<Button> options;
+    [SerializeField] private Color correctCol, wrongCol, normalCol;
+    private Question question;
+    private bool answered;
+    private float audioLength;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            Button localbtn = options[i];
+            localbtn.onClick.AddListener(() => OnClick(localbtn));
+        }
+    }
+
+    public void SetQuestion(Question question)
+    {
+        this.question = question;
+
+        switch (question.questionType)
+        {
+            case QuestionType.TEXT:
+                questionImage.transform.parent.gameObject.SetActive(false);
+                break;
+            case QuestionType.IMAGE:
+                TypeHolder();
+                questionImage.transform.gameObject.SetActive(true);
+                questionImage.sprite = question.questionImg;
+                break;
+            case QuestionType.VIDEO:
+                TypeHolder();
+                questionVid.transform.gameObject.SetActive(true);
+                questionVid.clip = question.questionVideo;
+                questionVid.Play();
+                break;
+            case QuestionType.AUDIO:
+                TypeHolder();
+                questionAudio.transform.gameObject.SetActive(true);
+                audioLength = question.questionClip.length;
+                StartCoroutine(PlayAudio());
+                break;
+        }
+        questionText.text = question.questionInfo;
+        List<string> answerList = ShuffleList.ShuffleListItems<string>(question.options);
+
+        for(int i=0; i<options.Count; i++)
+        {
+            options[i].GetComponentInChildren<Text>().text = answerList[i];
+            options[i].name = answerList[i];
+            options[i].image.color = normalCol;
+        }
+        answered = false;
+    }
+
+    IEnumerator PlayAudio()
+    {
+        if(question.questionType == QuestionType.AUDIO)
+        {
+            questionAudio.PlayOneShot(question.questionClip);
+
+            yield return new WaitForSeconds(audioLength + 0.5f);
+            StartCoroutine(PlayAudio());
+        }
+        else
+        {
+            StopCoroutine(PlayAudio());
+            yield return null;
+        }
+    }
+    void TypeHolder()
+    {
+        questionImage.transform.parent.gameObject.SetActive(true);
+        questionImage.transform.gameObject.SetActive(false);
+        questionVid.transform.gameObject.SetActive(false);
+        questionAudio.transform.gameObject.SetActive(false);
+    }
+
+    private void OnClick(Button btn)
+    {
+        if(!answered)
+        {
+            answered = true;
+            bool val = quizManager.Answer(btn.name);
+
+            if(val)
+            {
+                btn.image.color = correctCol;
+            }
+            else
+            {
+                btn.image.color = wrongCol;
+            }
+        }
+    }
+    
+}
